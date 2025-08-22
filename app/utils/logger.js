7290bb0967001ed45b7c09d45e1cc6cb5974a30e
alexandro priftuli <alexandro.priftuli@gmail.com>
2025-08-22 16:06:10 +0200

    added logger

---
+++ app/utils/logger.js
@@ -0,0 +1,10 @@
+/**
+ * Logs a message with a timestamp and optional log level.
+ * @param {string} message - The message to be logged.
+ * @param {string} [level='INFO'] - Log level (e.g., INFO, WARN, ERROR).
+ */
+export const log = (message, level = 'INFO') => {
+    // if (process.env.NODE_ENV !== 'production') { // Logs only in non-production environments
+        console.log(`[${level}]`, message)
+    // }
+}
